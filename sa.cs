7bfec8bf058629c5dd2e540ff4b34425d9186752
Nguyễn Trung Kiên <ntkien45-diendan@yahoo.com.vn>
2024-05-18 19:57:27 +0700

    Update sa.cs

--- sa.cs
+++ sa.cs
@@ -3,13 +3,9 @@
             try
             {
                 var s = Path.GetFullPath(path);
-
-                if (!Directory.Exists(path))
-                {
-                    s = Path.GetFileName(s);
-                    return !s.Any(p => Path.GetInvalidFileNameChars().Contains(p));
-                }
-                return true;
+                s = s.Substring(Path.GetPathRoot(s).Length);
+                var s2 = s.Split(Path.DirectorySeparatorChar);
+                return s2.All(item => !item.Any(p => Path.GetInvalidFileNameChars().Contains(p)));
             }
             catch (Exception)
             {
@@ -21,9 +17,56 @@
             if (CheckPathVaild(path))
             {
                 var s = Path.GetFullPath(path);
-                s = Path.GetFileName(s);
-                return !s.Any(p => Path.GetInvalidFileNameChars().Contains(p));
+                if(CheckHasFileInPath(s))
+                {
+                    return false;
+                }
+                if (!Directory.Exists(s))
+                {
+                    if (!s.EndsWith(Path.DirectorySeparatorChar))
+                    {
+                        s = Path.GetFileName(s);
+                        return !string.IsNullOrEmpty(s);
+                    }
+                }
             }
             return false;
         }
-   
+        private static bool CheckHasFileInPath(string path)
+        {
+            var s2 = path.Split(Path.DirectorySeparatorChar);
+            var s3 = s2[0];
+            for (int i = 1; i < s2.Length - 1; i++)
+            {
+                s3 = Path.Join(s3, s2[i]);
+                if (File.Exists(s3))
+                {
+                    FileAttributes attr = File.GetAttributes(s3);
+                    if (!attr.HasFlag(FileAttributes.Directory))
+                        return false;
+                }
+            }
+            return true;
+
+        }
+        public static bool CheckPathIsDirectory(string path)
+        {
+            if (CheckPathVaild(path))
+            {
+                var s = Path.GetFullPath(path);
+                if (CheckHasFileInPath(s))
+                {
+                    return false;
+                }
+                if (Directory.Exists(s))
+                {
+                    return true;
+                }
+                if (s.EndsWith(Path.DirectorySeparatorChar))
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+        }
